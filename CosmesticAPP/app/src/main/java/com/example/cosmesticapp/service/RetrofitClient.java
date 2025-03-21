8a6343cdee1dd8987d29480017c1ea66e3f69fbf
PhatBee <vinhphatst1235@gmail.com>
2025-03-21 08:19:48 +0700

    Add Dependency vs RetrofitClient

---
+++ CosmesticAPP/app/src/main/java/com/example/cosmesticapp/service/RetrofitClient.java
@@ -0,0 +1,27 @@
+package com.example.cosmesticapp.service;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    private static final String BASE_URL = "http://MyIP:8080/"; // Replace with your actual server URL
+    private static RetrofitClient instance;
+    private Retrofit retrofit;
+
+    private RetrofitClient() {
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .connectTimeout(60, TimeUnit.SECONDS)
+                .readTimeout(60, TimeUnit.SECONDS)
+                .writeTimeout(60, TimeUnit.SECONDS)
+                .build();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+}
