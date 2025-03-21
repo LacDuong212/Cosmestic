70979aa288d58c2e792b1f83199d105a3e5ca7b9
httdjuly <thuyduong3042@gmail.com>
2025-03-21 10:31:59 +0700

    update Intro Feature

--- CosmesticAPP/app/src/main/java/com/example/cosmesticapp/activity/LoginActivity.java
+++ CosmesticAPP/app/src/main/java/com/example/cosmesticapp/activity/LoginActivity.java
@@ -1,6 +1,5 @@
 package com.example.cosmesticapp.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -25,6 +24,10 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        SharedPreferences sharedPreferences = getSharedPreferences("user_prefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean("isLoggedIn", true); // Lưu trạng thái đăng nhập
+        editor.apply();
 
         registerText = findViewById(R.id.registerText);
         registerText.setOnClickListener(v -> {
