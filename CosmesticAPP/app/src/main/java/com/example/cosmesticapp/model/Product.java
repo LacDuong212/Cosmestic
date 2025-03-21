d21118d6bd5d8e96ea73ddbacca0702f00076fc3
Capybara <161992111+aMrnoob@users.noreply.github.com>
2025-03-21 10:15:06 +0700

    custom product

---
+++ CosmesticAPP/app/src/main/java/com/example/cosmesticapp/model/Product.java
@@ -0,0 +1,37 @@
+package com.example.cosmesticapp.model;
+
+public class Product {
+    private Long productId;
+    private String productName;
+    private String productImage;
+
+    public Product(Long productId, String productName, String productImage) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productImage = productImage;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductImage() {
+        return productImage;
+    }
+
+    public void setProductImage(String productImage) {
+        this.productImage = productImage;
+    }
+}
