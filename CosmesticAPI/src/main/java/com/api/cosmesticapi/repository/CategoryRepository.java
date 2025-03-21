9d0059320a740be2ca10cfcc5e2802e11e7cff4e
LacDuong <lacduongldg212@gmail.com>
2025-03-21 08:05:02 +0700

    Add API Category, Product

---
+++ CosmesticAPI/src/main/java/com/api/cosmesticapi/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.api.cosmesticapi.repository;
+
+import com.api.cosmesticapi.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
---
+++ CosmesticAPI/src/main/java/com/api/cosmesticapi/service/ProductService.java
@@ -0,0 +1,19 @@
+package com.api.cosmesticapi.service;
+
+
+import com.api.cosmesticapi.entity.Product;
+import com.api.cosmesticapi.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getProductsByCategoryOrderByPriceAsc(Long categoryId) {
+        return productRepository.findProductsByCategoryOrderByPriceAsc(categoryId);
+    }
+}
