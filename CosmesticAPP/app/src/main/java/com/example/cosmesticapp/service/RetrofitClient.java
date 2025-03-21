49b81aa1a1f295265484805a448c0b7d081c0909
PhatBee <vinhphatst1235@gmail.com>
2025-03-21 15:53:03 +0700

    fix bug Image

--- CosmesticAPP/app/src/main/java/com/example/cosmesticapp/service/RetrofitClient.java
+++ CosmesticAPP/app/src/main/java/com/example/cosmesticapp/service/RetrofitClient.java
@@ -9,7 +9,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "http://192.168.162.212:8080/";
+    private static final String BASE_URL = "http://192.168.224.57:8080/";
     private static RetrofitClient instance;
     private Retrofit retrofit;
 
