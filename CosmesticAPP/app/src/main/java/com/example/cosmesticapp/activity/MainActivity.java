f4ceaa31a68daaca3385f0d65259418b3a7fa3cb
httdjuly <thuyduong3042@gmail.com>
2025-03-21 10:36:23 +0700

    update login

--- CosmesticAPP/app/src/main/java/com/example/cosmesticapp/activity/MainActivity.java
+++ CosmesticAPP/app/src/main/java/com/example/cosmesticapp/activity/MainActivity.java
@@ -18,7 +18,7 @@ import com.google.android.material.snackbar.Snackbar;
 public class MainActivity extends AppCompatActivity {
     FloatingActionButton fab;
     BottomNavigationView botNav;
-
+    //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
