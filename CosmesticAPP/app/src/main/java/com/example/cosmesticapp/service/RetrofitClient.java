5b9e8828eedc277c2ff6612905fb54ee091d80d1
Vi021 <hellno83737@gmail.com>
2025-03-21 09:14:11 +0700

    add and change some files

--- CosmesticAPP/app/src/main/java/com/example/cosmesticapp/service/RetrofitClient.java
+++ CosmesticAPP/app/src/main/java/com/example/cosmesticapp/service/RetrofitClient.java
@@ -24,4 +24,15 @@ public class RetrofitClient {
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
     }
+
+    public static Retrofit getClient(String baseUrl) {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(baseUrl)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        return retrofit;
+    }
 }
